36972b00e20d0ccfcb5e1feb5c14b666e40e3f78
Vitor Madeira <vitorsoutom@hotmail.com>
2026-05-23 09:10:47 -0300

    feat: adds authentication middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,12 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+export const authenticate = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> => {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.status(401).send({ message: "Token inválido ou expirado" });
+  }
+};
