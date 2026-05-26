02fb29e1e11aaa3c9e94e1488077efde7a7a68f5
Vitor Madeira <vitorsoutom@hotmail.com>
2026-05-26 10:06:20 -0300

    fix: reactivate authentication

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -6,10 +6,10 @@ import {
   listProducts,
   updateExistingProduct,
 } from "../controllers/products.controller";
-//import { authenticate } from "../middlewares/auth.middleware";
+import { authenticate } from "../middlewares/auth.middleware";
 
 export default async function productRoutes(fastify: FastifyInstance) {
-  // fastify.addHook("onRequest", authenticate);
+  fastify.addHook("onRequest", authenticate);
   fastify.get(
     "/",
     {
